8456732d92a516077bb569c9291a6e24daa45376
Winstone2 <117536588+Winstone2@users.noreply.github.com>
2022-11-18 00:54:20 +0300

    Commit 7

--- task111/helper.js
+++ task111/helper.js
@@ -1,3 +1,5 @@
-export const user = {
-  age: '100',
-};
+export const user = {};
+user.age = 100;
+user.name = 'Winstone';
+user.surname = 'Owino';
+user.address = '11/22';
--- task115/helper.js
+++ task115/helper.js
@@ -1,1 +1,17 @@
-const objects = [];
+export const objects = [{
+
+        Name: 'Winstone',
+        Age: '62',
+        mobile: 704131845
+    },
+    {
+        Name: 'Gilbert',
+        Age: '40',
+        mobile: 711232111
+    },
+    {
+        Name: 'Millicent',
+        Age: '23',
+        mobile: 704212121
+    }
+];
